6dab14cdd0950be889dba7478f928f3cfc94a2de
Atif-1 <atif1712001@gmail.com>
2024-07-02 23:22:19 +0530

    ...

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const path=require("path");
 const express=require("express");
 const app=express();
 
 app.use(express.json());
 app.use('/',(req,res)=>{
-	res.sendFile("./index.html");
+	res.sendFile(path.join(__dirname,`/index.html`));
 });
 
 app.listen(3000);
